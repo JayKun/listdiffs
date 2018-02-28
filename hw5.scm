744be001c0569485b3043cce86acdba58b55cc2e
JayKun <junkaiong11@gmail.com>
2018-02-28 11:31:40 -0800

    null-ld works

---
+++ hw5.scm
@@ -0,0 +1,12 @@
+(define ils (append '(a e i o u) 'y))
+(define d1 (cons ils (cdr (cdr ils))))
+(define d2 (cons ils ils))
+(define d3 (cons ils (append '(a e i o u) 'y)))
+(define d4 (cons '() ils))
+(define d5 0)
+
+
+(define (null-ld? obj) 
+(eq? (car obj) (cdr obj)))
+
+
